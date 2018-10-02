8bcceebcdf076eec0cea5ab1ae3a273c8dc607b7
George Self <selfg@cochise.edu>
2018-10-02 13:45:48 -0700

    Copied from original site
    Copied the site files from original site to repository.

---
+++ Originals/scripts/_vti_cnf/graph-calc.js
@@ -0,0 +1,13 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|03 Mar 2012 01:41:26 -0000
+vti_extenderversion:SR|12.0.0.4518
+vti_author:SR|George-Home\\George
+vti_modifiedby:SR|George-Home\\George
+vti_timecreated:TR|19 Feb 2012 18:35:34 -0000
+vti_backlinkinfo:VX|
+vti_syncwith_74.52.20.77\:21/%2fpublic_html:TX|19 Feb 2012 18:35:34 -0000
+vti_syncofs_74.52.20.77\:21/%2fpublic_html:TW|20 Feb 2012 15:43:00 -0000
+vti_nexttolasttimemodified:TR|03 Mar 2012 01:40:51 -0000
+vti_cacheddtm:TX|19 Feb 2012 18:35:34 -0000
+vti_filesize:IR|1200
+vti_donotpublish:BW|true
